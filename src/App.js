e8f8191060228fc19d5983b21511604639b57476
github username <github email address>
2021-04-27 22:45:56 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './components/layout.js/Header';
+import { Container } from 'react-bootstrap';
+import Home from './components/pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Header />
+				<Container>
+					<Switch>
+						<Route path="/" component={Home} />
+					</Switch>
+				</Container>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/layout.js/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+	return (
+		<Navbar bg="light" expand="lg">
+			<Navbar.Brand>
+				<Link>Ahava Hope Foundation</Link>
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link>
+						<Link to="/">Home</Link>
+					</Nav.Link>
+					<Nav.Link>
+						<Link to="/about">About Us</Link>
+					</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
+};
+
+export default Header;
